5a77c926a9bc0eb6bb25151ada6c7064523329a2
stevesherrin <steve@sherrinentertainment.com>
2016-06-01 10:18:30 -0400

    Basic Structure
    Basic Structure

--- index.php
+++ index.php
@@ -4,15 +4,31 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>QPC Card Builder -- Home</title>
 
     <!-- Bootstrap -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,700italic,900,900italic|Oswald:400,700|Indie+Flower|Bitter:400,400italic,700|Passion+One:400,700,900|Anton|Libre+Baskerville:400,400italic,700|Crimson+Text:400,600,600italic,700,700italic|Play:400,700|Francois+One|Questrial|Orbitron:400,500,700,900|Dancing+Script:400,700|Sigmar+One|Chewy|Work+Sans:400,600,500,700,800,900|Lobster+Two:400,400italic,700,700italic|Archivo+Black|Bevan|Alfa+Slab+One|Great+Vibes|Tangerine:400,700' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/mystyles.css" rel="stylesheet">
 
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body id="home">
+  	<section class="container">
+    
+    	<div class="content row">
+        
+        	<section class="tools col-lg-12">
+            	<h2>Tools</h2>
+            </section><!-- TOOLS -->
+            
+        	<section class="main col-lg-8">
+            	<h2>MAIN</h2>
+                <p>bsbdf kjbSDF S FBKJSDFB SDFJB KSDF BKJSF BKSDF JBKDSF BSDF JKDSBFKBS DF SJDKFKLSDF BKJDSFKJBLSDF KJDSFBKBDSFLKSDF</p>
+            </section> <!-- MAIN -->
+            
+        </div> <!-- CONTENT -->
+        
+   	</section> <!-- CONTAINER -->
 
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscripts.js"></script>
