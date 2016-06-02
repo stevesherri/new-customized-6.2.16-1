f623ae6d2bda32bf7140071c3769d4f248dd55cf
stevesherrin <steve@sherrinentertainment.com>
2016-06-02 07:58:42 -0400

    old
    old

---
+++ index.php
@@ -0,0 +1,9 @@
+<? include("header.php"); ?>
+
+        	<section class="main col-lg-8">
+            	<h2>MAIN</h2>
+                <p>bsbdf kjbSDF S FBKJSDFB SDFJB KSDF BKJSF BKSDF JBKDSF BSDF JKDSBFKBS DF SJDKFKLSDF BKJDSFKJBLSDF KJDSFBKBDSFLKSDF</p>
+            </section> <!-- MAIN -->
+            
+   <? include("footer.php"); ?>
+     
