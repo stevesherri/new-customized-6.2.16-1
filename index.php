21d48ba1c7837d6345ecd960bef52e9bfcb4f8a9
stevesherrin <steve@sherrinentertainment.com>
2016-06-02 12:13:19 -0400

    1.001
    1.001

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
         </div> <!-- END PHONE BAR -->
         
         <header class="container">
-        	<section class="col-sm-2">
+        	<section class="col-xs-12 col-sm-2">
             	 <img src="images/QPC_Logo.png" style="max-height:50px; margin:5px;"/>
             </section>
-            <section class="topnav col-lg-10">	
+            <section class="topnav col-xs-12 col-sm-10">	
             	<div class="navbar navbar-right">
                 	<ul class="nav navbar-nav">
                     	<li><a href="#">Add An Image</a></li>
@@ -49,10 +49,10 @@
                             </ol>
                         	<div class="carousel-inner">
                             	<div class="item active">
-                                	<img class="img-rounded-bottom" src="images/carousel/Playing-cards-banner.jpg" alt="Low 10 Deck Minimum"/>
+                                	<img class="img-rounded-bottom img-responsive" src="images/carousel/Playing-cards-banner.jpg" alt="Low 10 Deck Minimum"/>
                                 </div><!-- END SLIDE 1 -->
                                 <div class="item">
-                                	<a href="#"><img class="img-rounded-bottom" src="images/carousel/Custom-playing-cards-banne2.jpg" alt="Get A Quote"/></a>
+                                	<a href="#"><img class="img-rounded-bottom img-responsive" src="images/carousel/Custom-playing-cards-banne2.jpg" alt="Get A Quote"/></a>
                                 </div><!-- END SLIDE 2 -->
                             </div> <!-- END CAROUSEL INNER -->
                             <!--
@@ -67,7 +67,29 @@
                 </div><!-- END ROW -->
             </div> <!-- END CONTENT CONTAINER -->
         </section> <!-- SLIDE SHOW -->
+        <section class="container">
+        	<div class="col-md-8 col-sm-12 text-right">
+            	<h2 id="offer-contact">We offer the best prices for all your needs!</h2>
+            </div>
+            <div class="col-md-4 col-sm-12">
+            	<button class="btn-danger btn-lg col-xs-12 col-md-12"  href="#">Get a quote now!!!</button>
+            </div>
+        </section><!-- END OFFER QUOTE BAR -->
+        <hr/>
+        <section class="container">
         
+        </section><!-- END PAGE CONTENT -->
+        <section style="background-color:#1c5ba8">
+            <footer class="row">
+                <nav class="col-lg-12">
+                    <ul class="mybreadcrumb breadcrumb text-center " style="background:none; background-color:none;margin-top:10px;">
+                        <li><a href="#">One</a></li>
+                        <li><a href="#">Two</a></li>
+                        <li><a href="#">Three</a></li>
+                    </ul><!-- BREADCRUMBS -->
+                </nav><!-- NAV -->
+            </footer><!-- FOOTER -->
+        </section> <!-- CONTAINER -->
         <script src="js/bootstrap.js"></script>
         <script src="js/myscripts.js"></script>
     </body>
